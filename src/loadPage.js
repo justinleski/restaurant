9eb2b0ce9789b1784bd8273f58fa03b9fd849318
Justin <o0justinsgamez0o@gmail.com>
2024-07-17 21:13:00 -0400

    feat: Add home screen

--- src/loadPage.js
+++ src/loadPage.js
@@ -5,15 +5,25 @@ const initLoad = () => {
     var wrapper = document.createElement("div");
 
     // Make wrapper for main content
-    var title = document.createElement("h1");
-    title.textContent = "Placeholder Cool Name";
+    var title = document.createElement("h2");
+    title.textContent = "Fortune favours \nthe flavourful";
     wrapper.appendChild(title);
 
-    // 
-    var pg = document.createElement("p");
-    pg.textContent = "Lorem ipsum";
-    wrapper.appendChild(pg);
+    // Create paragraph and call to action
+    var card = document.createElement("div");
 
+    var pg = document.createElement("p");
+    pg.textContent = "Welcome to Le Trèfle, a Quebec-based restaurant which serves authentic dishes from passed down from generations of French immigrants. We pride ourselves in our roots and the flavours that they brought us.";
+    card.appendChild(pg);
+
+    const callToAct = document.createElement("button");
+    callToAct.textContent = "Learn More";
+    callToAct.classList.add("actionBtn");
+    card.appendChild(callToAct);
+
+    // 
+    wrapper.appendChild(card);
+    wrapper.classList.add("section")
     return wrapper;
 }
 
