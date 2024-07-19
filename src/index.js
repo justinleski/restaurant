fb4d58bc70a0e08248c837877ed60cdec062f1b6
Justin <o0justinsgamez0o@gmail.com>
2024-07-19 12:40:13 -0400

    feat: Nav highlights current selected tab

--- src/index.js
+++ src/index.js
@@ -13,16 +13,35 @@ mainCont.appendChild(menuPrev());
 const navButtons = document.querySelectorAll("nav button");
 
 navButtons.forEach(button => {
+
+
+
     button.addEventListener("click", () => {
+        
         var pageAction = button.getAttribute("data-page-type");
+
+        // 
+        clearContent(mainCont); // Clears page of current content
+
+        // Displays current selected tab
+        navButtons.forEach(btn => {
+            if (btn.getAttribute("data-page-type") == pageAction){
+                btn.style.background = "#89D69A";
+            }
+            else {
+                btn.style.background = "transparent";
+            }
+        })
+        
         switch(pageAction) {
             case "home":
                 // init load and menupreview
+                mainCont.appendChild(initLoad());
+                mainCont.appendChild(menuPrev());
                 break;
             case "menu":
-                clearContent(mainCont);
                 mainCont.appendChild(menuHead());
-                break
+                break;
             case "about":
                 //
                 break;
@@ -30,6 +49,8 @@ navButtons.forEach(button => {
                 //
                 break;
         }
+
+        
     });
 });
 
